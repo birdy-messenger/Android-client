ea6fa199ebb089f6a95386d1c299d560f2cb1721
Aroize <gipermonk@bk.ru>
2019-06-12 01:07:51 +0300

    Register page

---
+++ app/src/main/java/com/birdyteam/birdyandroidversion/RegisterActivity.kt
@@ -0,0 +1,25 @@
+package com.birdyteam.birdyandroidversion
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+ * @author: Ilia Ilmenskii
+ */
+class RegisterActivity : AppCompatActivity() {
+
+    companion object {
+        fun getInstance(packageContext : Context) : Intent {
+            return Intent(packageContext, RegisterActivity::class.java)
+        }
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        supportActionBar?.setTitle(R.string.sign_up)
+    }
+}
