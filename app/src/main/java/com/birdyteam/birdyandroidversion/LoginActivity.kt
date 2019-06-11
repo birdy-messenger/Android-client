cf1f664d914ebbd5ef27923e17d743fdb95db63a
Aroize <gipermonk@bk.ru>
2019-06-12 01:04:27 +0300

    Added move to RegisterActivity

--- app/src/main/java/com/birdyteam/birdyandroidversion/LoginActivity.kt
+++ app/src/main/java/com/birdyteam/birdyandroidversion/LoginActivity.kt
@@ -2,15 +2,25 @@ package com.birdyteam.birdyandroidversion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 /*
  * @author: Ilia Ilmenskii
  */
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var registerBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
         supportActionBar?.hide()
+
+        registerBtn = findViewById(R.id.register_btn)
+        registerBtn.setOnClickListener {
+            startActivity(
+                RegisterActivity.getInstance(this)
+            )
+        }
     }
 }
