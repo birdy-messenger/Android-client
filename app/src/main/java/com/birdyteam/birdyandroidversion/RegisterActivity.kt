a2705f19a2034380e2a0e57ce038d7d1eedc9c1e
Aroize <gipermonk@bk.ru>
2019-06-27 01:49:16 +0300

    Added request for auth and parsing response

--- app/src/main/java/com/birdyteam/birdyandroidversion/RegisterActivity.kt
+++ app/src/main/java/com/birdyteam/birdyandroidversion/RegisterActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 /*
  * @author: Ilia Ilmenskii
@@ -16,11 +18,25 @@ class RegisterActivity : AppCompatActivity() {
         }
     }
 
+    private lateinit var registerBtn : Button
+    private lateinit var enterName : EditText
+    private lateinit var enterEmail : EditText
+    private lateinit var enterPassword : EditText
+    private lateinit var confirmPassword : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         supportActionBar?.hide()
+
+        enterName = findViewById(R.id.enter_name)
+        enterEmail = findViewById(R.id.enter_email)
+        enterPassword = findViewById(R.id.enter_password)
+        confirmPassword = findViewById(R.id.confirm_password)
+
+        registerBtn = findViewById(R.id.register_btn)
+        registerBtn.setOnClickListener {
+        }
     }
 
     override fun onBackPressed() {
