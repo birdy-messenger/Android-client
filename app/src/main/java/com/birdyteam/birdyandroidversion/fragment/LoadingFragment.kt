6824ed40ba2d6f509b9a469bfc1769963267a8c9
Aroize <gipermonk@bk.ru>
2019-07-08 01:26:48 +0300

    Spinning while request is active fragment

---
+++ app/src/main/java/com/birdyteam/birdyandroidversion/fragment/LoadingFragment.kt
@@ -0,0 +1,22 @@
+package com.birdyteam.birdyandroidversion.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.birdyteam.birdyandroidversion.R
+import dagger.Module
+
+/**
+ * @project Android-client
+ * @author Ilia Ilmenskii created on 07.07.2019
+ */
+
+@Module
+class LoadingFragment : DialogFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_loading, container, false)
+    }
+}
