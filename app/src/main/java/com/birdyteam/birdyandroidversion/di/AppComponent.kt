b98163bd6ab9af37a555da2711588aabb7817025
Aroize <gipermonk@bk.ru>
2019-07-08 02:40:35 +0300

    RegisterPresenter injected

--- app/src/main/java/com/birdyteam/birdyandroidversion/di/AppComponent.kt
+++ app/src/main/java/com/birdyteam/birdyandroidversion/di/AppComponent.kt
@@ -4,6 +4,7 @@ import com.birdyteam.birdyandroidversion.activity.LoginActivity
 import com.birdyteam.birdyandroidversion.di.modules.LoadingModule
 import com.birdyteam.birdyandroidversion.di.modules.NetworkModule
 import com.birdyteam.birdyandroidversion.presenter.LoginPresenter
+import com.birdyteam.birdyandroidversion.presenter.RegisterPresenter
 import dagger.Component
 import javax.inject.Singleton
 
@@ -12,4 +13,5 @@ import javax.inject.Singleton
 interface AppComponent {
     fun inject(activity : LoginActivity)
     fun inject(presenter: LoginPresenter)
+    fun inject(presenter: RegisterPresenter)
 }
