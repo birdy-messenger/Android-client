f1f211b4f0b58e339e478d8538c5aa745a4a048a
Aroize <gipermonk@bk.ru>
2019-07-08 01:29:28 +0300

    String extension function for MD5 create

---
+++ app/src/main/java/com/birdyteam/birdyandroidversion/md5/MD5Hash.kt
@@ -0,0 +1,19 @@
+package com.birdyteam.birdyandroidversion.md5
+
+import java.math.BigInteger
+import java.security.MessageDigest
+
+/**
+ * @project Android-client
+ * @author Ilia Ilmenskii created on 08.07.2019
+ */
+
+fun String.createMD5() : String {
+    val md : MessageDigest = MessageDigest.getInstance("MD5")
+    md.reset()
+    md.update(this.toByteArray())
+    val digest = md.digest()
+    return BigInteger(1, digest)
+        .toString(16)
+        .padStart(32, '0')
+}
