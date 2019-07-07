d5a6e91afee87304e83c089874f2e0d3d0321f5e
Aroize <gipermonk@bk.ru>
2019-07-08 01:32:36 +0300

    Serialized responses from Birdy server

---
+++ app/src/main/java/com/birdyteam/birdyandroidversion/network/response/AuthResponse.kt
@@ -0,0 +1,18 @@
+package com.birdyteam.birdyandroidversion.network.response
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+/**
+ * @project Android-client
+ * @author Ilia Ilmenskii created on 08.07.2019
+ */
+
+data class AuthResponse(
+    @SerializedName ("id")
+    @Expose
+    val id : Int,
+    @SerializedName ("token")
+    @Expose
+    val token : Long
+)
---
+++ app/src/main/java/com/birdyteam/birdyandroidversion/network/response/RegResponse.kt
@@ -0,0 +1,14 @@
+package com.birdyteam.birdyandroidversion.network.response
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+/**
+ * @project Android-client
+ * @author Ilia Ilmenskii created on 08.07.2019
+ */
+data class RegResponse(
+    @SerializedName ("result")
+    @Expose
+    val result : String
+)
