aedb1eb229b142af7b49637c3a0b39ead229eafe
Aroize <gipermonk@bk.ru>
2019-07-08 01:26:02 +0300

    Requests to server with 'app' methods

---
+++ app/src/main/java/com/birdyteam/birdyandroidversion/network/api/AppRequests.kt
@@ -0,0 +1,28 @@
+package com.birdyteam.birdyandroidversion.network.api
+
+import com.birdyteam.birdyandroidversion.network.response.AuthResponse
+import com.birdyteam.birdyandroidversion.network.response.RegResponse
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+/**
+ * @project Android-client
+ * @author Ilia Ilmenskii created on 08.07.2019
+ */
+
+interface AppRequests {
+    @GET ("/api/app/auth")
+    fun auth(
+        @Query ("email") email : String,
+        @Query ("passwordHash") password : String
+    ) : Single<AuthResponse>
+
+    @POST ("/api/app/reg")
+    fun reg(
+        @Query ("email") email: String,
+        @Query ("passwordHash") password: String,
+        @Query ("firstName") name : String
+    ) : Single<RegResponse>
+}
