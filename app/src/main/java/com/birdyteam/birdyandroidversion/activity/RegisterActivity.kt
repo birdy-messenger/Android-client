8d814ecca91d0ea0feecb8b7af97d81bd330e6d7
Aroize <gipermonk@bk.ru>
2019-07-08 02:44:11 +0300

    Implemented MvpView and injected presenter

--- app/src/main/java/com/birdyteam/birdyandroidversion/activity/RegisterActivity.kt
+++ app/src/main/java/com/birdyteam/birdyandroidversion/activity/RegisterActivity.kt
@@ -1,10 +1,39 @@
 package com.birdyteam.birdyandroidversion.activity
 
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import com.arellomobile.mvp.MvpAppCompatActivity
+import com.arellomobile.mvp.presenter.InjectPresenter
+import com.birdyteam.birdyandroidversion.R
+import com.birdyteam.birdyandroidversion.presenter.RegisterPresenter
+import com.birdyteam.birdyandroidversion.view.RegisterView
 
 /**
  * @project Android-client
  * @author Ilia Ilmenskii created on 07.07.2019
  */
-class RegisterActivity : MvpAppCompatActivity() {
+class RegisterActivity : MvpAppCompatActivity(), RegisterView {
+    companion object {
+        fun getInstance(packageContext: Context) = Intent(packageContext, RegisterActivity::class.java)
+    }
+
+    @InjectPresenter
+    lateinit var registerPresenter : RegisterPresenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        supportActionBar?.hide()
+
+
+    }
+
+    /**
+     * @interface : RegisterView implementation
+     */
+
+    override fun registerClicked() {
+
+    }
 }
