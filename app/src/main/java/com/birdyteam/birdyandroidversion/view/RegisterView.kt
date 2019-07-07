14d98e2265a8ec0f75432c58592a9a8893024e89
Aroize <gipermonk@bk.ru>
2019-07-08 02:44:35 +0300

    Implemented MvpView and injected presenter

---
+++ app/src/main/java/com/birdyteam/birdyandroidversion/view/RegisterView.kt
@@ -0,0 +1,11 @@
+package com.birdyteam.birdyandroidversion.view
+
+import com.arellomobile.mvp.MvpView
+
+/**
+ * @project Android-client
+ * @author Ilia Ilmenskii created on 08.07.2019
+ */
+interface RegisterView : MvpView {
+    fun registerClicked()
+}
