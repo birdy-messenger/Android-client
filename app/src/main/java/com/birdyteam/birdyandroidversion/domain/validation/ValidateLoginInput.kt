8ef592c9dbe388401ccd6992d1f88a7d2b5c732b
Aroize <gipermonk@bk.ru>
2019-07-09 21:10:47 +0300

    Changed result of validation to sealed class

--- app/src/main/java/com/birdyteam/birdyandroidversion/domain/validation/ValidateLoginInput.kt
+++ app/src/main/java/com/birdyteam/birdyandroidversion/domain/validation/ValidateLoginInput.kt
@@ -1,25 +1,34 @@
 package com.birdyteam.birdyandroidversion.domain.validation
 
+import android.content.res.Resources
+import com.birdyteam.birdyandroidversion.R
 import com.birdyteam.birdyandroidversion.data.network.api.app.input.LoginInput
 
 /**
  * @project Android-client
  * @author Ilia Ilmenskii created on 09.07.2019
  */
-class ValidateLoginInput {
+class ValidateLoginInput(
+    resources: Resources
+) {
+    private val errors = resources.getStringArray(R.array.validation_errors)
+
     fun validate(input: LoginInput): ValidationResult {
-        if(!input.email.matches(Regex(InputPatterns.EMAIL_PATTERN)))
-            return ValidationResult.EMAIL_NOT_MATCH
-        if(!input.password.matches(Regex(InputPatterns.PASSWORD_PATTERN))) {
+        if (!input.email.matches(Regex(InputPatterns.EMAIL_PATTERN)))
+            return ValidationError(errors[0])
+        if (!input.password.matches(Regex(InputPatterns.PASSWORD_PATTERN))) {
             return when (input.password.length) {
                 in 0..5 -> {
-                   ValidationResult.TOO_SHORT_PASSWORD
+                    ValidationError(errors[1])
+                }
+                in 6..32 -> {
+                    ValidationError(errors[2])
                 }
                 else -> {
-                    ValidationResult.TOO_LONG_PASSWORD
+                    ValidationError(errors[3])
                 }
             }
         }
-        return ValidationResult.SUCCESS
+        return ValidationSuccess
     }
 }
