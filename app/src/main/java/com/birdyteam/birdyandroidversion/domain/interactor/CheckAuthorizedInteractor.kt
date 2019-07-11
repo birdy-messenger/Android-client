321f0fa4a4bb679b4e801fa0eb769a72908a2909
Aroize <gipermonk@bk.ru>
2019-07-12 00:35:31 +0300

    Login saving and reading info using shared preferences

---
+++ app/src/main/java/com/birdyteam/birdyandroidversion/domain/interactor/CheckAuthorizedInteractor.kt
@@ -0,0 +1,18 @@
+package com.birdyteam.birdyandroidversion.domain.interactor
+
+import com.birdyteam.birdyandroidversion.data.network.api.app.response.AuthResponse
+import com.birdyteam.birdyandroidversion.data.repository.UserAuthInfoRepository
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * @project Android-client
+ * @author Ilia Ilmenskii created on 12.07.2019
+ */
+class CheckAuthorizedInteractor(
+    private val userAuthInfoRepository: UserAuthInfoRepository
+) {
+    fun checkSignIn(): Single<AuthResponse> = userAuthInfoRepository
+        .getAuthInfo()
+        .subscribeOn(Schedulers.io())
+}
